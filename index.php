7b2dfb9bbb5f9ce5a254f41caedeac4b926322f3
Akshay P M <akshay.prabhakaran@qburst.com>
2023-11-08 17:06:30 +0530

    kk

--- index.php
+++ index.php
@@ -88,7 +88,86 @@
 // var_dump($array[1]["title"]);
 
 //! accessing and processing each element of a array
-$array = array("First Post", "Middle Post", "Last Post");
-foreach($array as $article) {
-    echo $article, ". ";
-} // no semi colon is needed at the end of a code block. 
+// $array = array("First Post", "Middle Post", "Last Post");
+// foreach($array as $article) {
+//     echo $article, ". ";
+// } // no semi colon is needed at the end of a code block. 
+
+// foreach ($array as $key => $value) {
+//     echo $key ." ". $value;
+// }
+
+//! if loop
+/* empty() function return true or false*/
+// $array = [];
+
+// if (empty($array) ) {
+//     echo "The array is empty";
+// } else {
+//     echo "The array is not empty";
+// }
+
+//! comparison operators
+// var_dump(3 == 4);
+// var_dump(5 == 5);
+// var_dump(6 != 6);
+
+//! while loop
+// $month = 1;
+
+// while($month <= 12) {
+//     echo $month . ", "; // $month will automatically converted to string.
+//     $month = $month + 1;
+// }
+
+//! for loop
+// for ($i = 0; $i <= 10; $i++) {
+//     echo $i .", ";
+// }
+
+//! switch statements
+// $day = "mon";
+
+// switch ($day) {
+//     case "mon":
+//         echo "Monday";
+//         break;
+//     case "tue":
+//         echo "Tuesday";
+//         break;
+//     case "wed":
+//         echo "Wednesday";
+//         break;
+//     case "thu":
+//         echo "Thursday";
+//         break;
+//     case "fri":
+//         echo "Friday";
+//         break;
+//     case "sat":
+//         echo "Saturday";
+//         break;
+//     case "sun":
+//         echo "Sunday";
+//         break;
+//     default:
+//         echo "Please enter a valid day.";
+//         break;
+// }
+
+
+
+$array = [];
+
+for ($i = 1; $i <= 10; $i++) {
+
+    if($i < 4) {
+        $array[] = "a";
+    }elseif ($i > 3 and $i < 8) {
+        $array[] = "b";
+    }elseif($i >= 8) {
+        $array[] = "c";
+    }
+}
+
+echo $array;
