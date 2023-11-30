0e9cf46986b78ae2caab30ed3ba2adcb6aa43fb0
Akshay P M <ambadiakshay25@gmail.com>
2023-12-01 00:57:13 +0530

    added combination of search

--- Assignment/js/index.js
+++ Assignment/js/index.js
@@ -1,7 +1,11 @@
 $(document).ready(function () {
+    let search_collection = {};
+    
     $(".search").on("keyup", function (e) {
         e.preventDefault();
 
+
+        let table_attr = this.id
         // to get the initail page 
         const urlParams = new URLSearchParams(window.location.search);
         let pageNo = urlParams.get('pages');
@@ -10,100 +14,106 @@ $(document).ready(function () {
             pageNo = 1
         }
 
-        let table_attr = this.id
         let search_term = $("#" + table_attr).val()
+        search_collection[table_attr] = "%"+search_term+"%"
 
         // stroing in session for pagining purpose
-        sessionStorage.setItem('table_attr', table_attr)
-        sessionStorage.setItem('search_term', search_term)
+        // sessionStorage.setItem('search', search_collection)
+        // sessionStorage.setItem('search_term', search_term)
+        console.log(search_collection)
+        // $("#" + table_attr).on("keyup", function () {
 
-        if (search_term != "") {
+        // })
 
-            // sending an ajax request to the search.php
-            // to get the search result back
-            $.ajax({
+        if (!$.isEmptyObject(search_collection)) {
 
-                url: "search.php",
-                method: "POST",
-                data: {
-                    table_attr: this.id,
-                    search_term: search_term,
-                    default: 'false',
-                    page: pageNo
-                },
-                success: function (data) {
-                    $("tbody").html(data)
-                },
-                error: function (err) {
-                    console.log(err)
-                },
-            });
+            console.log("arr")
+                // sending an ajax request to the search.php
+                // to get the search result back
+                $.ajax({
 
-            // sending a request to navigator.php to get the correct pagination buttom navigation
-            $.ajax({
+                    url: "search.php",
+                    method: "POST",
+                    data: {
+                        table_attr: this.id,
+                        search_term: search_collection,
+                        default: 'false',
+                        page: pageNo
+                    },
+                    success: function (data) {
+                        $("tbody").html(data)
+                        // console.log(data)
+                    },
+                    error: function (err) {
+                        console.log(err)
+                    },
+                });
 
-                url: "search.php",
-                method: "POST",
-                data: {
-                    table_attr: this.id,
-                    search_term: search_term,
-                    pagenation: "true",
-                    default: 'false',
-                    page: pageNo
-                },
-                success: function (data) {
-                    $("nav").replaceWith(data);
-                },
-                error: function (err) {
-                    console.log(err)
-                },
-            });
+                // sending a request to navigator.php to get the correct pagination buttom navigation
+                $.ajax({
 
-        } else {
+                    url: "search.php",
+                    method: "POST",
+                    data: {
+                        table_attr: this.id,
+                        search_term: search_collection,
+                        pagenation: "true",
+                        default: 'false',
+                        page: pageNo
+                    },
+                    success: function (data) {
+                        $("nav").replaceWith(data);
+                    },
+                    error: function (err) {
+                        console.log(err)
+                    },
+                });
 
-            // if search term is removed from the section
-            // then to show the orginal table a request is send to
-            // default.php
-            $.ajax({
+            } else {
 
-                url: "search.php",
-                method: "POST",
-                data: {
-                    default: 'true',
-                    page: pageNo
-                },
-                success: function (data) {
-                    $("tbody").html(data)
-                },
-                error: function (err) {
-                    console.log(err)
-                },
-            });
+                // if search term is removed from the section
+                // then to show the orginal table a request is send to
+                // default.php
+                $.ajax({
 
-            // sending a request to navigator.php to get the correct pagination buttom navigation
-            // for corresponding default page
-            $.ajax({
+                    url: "search.php",
+                    method: "POST",
+                    data: {
+                        default: 'true',
+                        page: pageNo
+                    },
+                    success: function (data) {
+                        $("tbody").html(data)
+                    },
+                    error: function (err) {
+                        console.log(err)
+                    },
+                });
 
-                url: "search.php",
-                method: "POST",
-                data: {
-                    pagenation: "true",
-                    default: 'false',
-                    page: pageNo
-                },
-                success: function (data) {
-                    $("nav").replaceWith(data);
-                },
-                error: function (err) {
-                    console.log(err)
-                },
-            });
-        }
-        var uri = window.location.href.toString();
-        if (uri.indexOf("?") > 0) {
-            var clean_uri = uri.substring(0, uri.indexOf("?"));
-            window.history.replaceState({}, document.title, clean_uri);
-        }
+                // sending a request to navigator.php to get the correct pagination buttom navigation
+                // for corresponding default page
+                $.ajax({
+
+                    url: "search.php",
+                    method: "POST",
+                    data: {
+                        pagenation: "true",
+                        default: 'false',
+                        page: pageNo
+                    },
+                    success: function (data) {
+                        $("nav").replaceWith(data);
+                    },
+                    error: function (err) {
+                        console.log(err)
+                    },
+                });
+            }
+            var uri = window.location.href.toString();
+            if (uri.indexOf("?") > 0) {
+                var clean_uri = uri.substring(0, uri.indexOf("?"));
+                window.history.replaceState({}, document.title, clean_uri);
+            }
     });
 
 
